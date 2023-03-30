85f2070b23c772d80d0e36fee8d1a9a7284be918
Italo Lon kan <sanchotorres777@gmail.com>
2023-03-29 22:01:52 -0500

    Update greetings controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
-    @greeting = Greeting.all.order('random()').first
-    render json: { greeting: @greeting.message }
+    if Greeting.count.zero?
+      render json: { greeting: 'THERE ARE NOT GREETINGS ON THE API. PLEASE ADD THEM FROM BACKEND' }
+    else
+      @greeting = Greeting.all.order('random()').first
+      render json: { greeting: @greeting.message }
+    end
   end
 end
